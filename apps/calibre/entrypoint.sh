ee7751718bb2af7379396da46ce80710eba6e5d1
Cayce House <cayce@caycehouse.com>
2024-05-09 23:38:07 -0400

    add calibre

---
+++ apps/calibre/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [[ "${CALIBRE_CREATE_LIBRARY}" == "true" && ! -f "${CALIBRE_LIBRARY}/metadata.db" ]]; then
+    /app/calibredb --library-path="${CALIBRE_LIBRARY}" list
+fi
+
+exec \
+    /app/calibre-server \
+        --port=${CALIBRE_PORT} \
+        ${CALIBRE_LIBRARY} \
+        "$@"
