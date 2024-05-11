8b23cb86258bac385fb8507ab0a5fd9d6240af9e
Cayce House <cayce@caycehouse.com>
2024-05-10 22:07:45 -0400

    add spt

---
+++ apps/spt/ci/latest.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+version=$(curl -sX GET "https://dev.sp-tarkov.com/api/v1/repos/SPT-AKI/Server/tags" | jq --raw-output '.[0].name' 2>/dev/null)
+version="${version#*v}"
+version="${version#*release-}"
+printf "%s" "${version}"
