a46b9b819e5433311de9dfe3c6984d3237c53f10
Cayce House <cayce@caycehouse.com>
2024-05-18 23:29:27 -0400

    add v to version

--- apps/talosctl/ci/latest.sh
+++ apps/talosctl/ci/latest.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 version=$(curl -sX GET "https://api.github.com/repos/siderolabs/talos/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)
-version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
