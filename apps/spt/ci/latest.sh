f7974493f5a621143226a77928cb6f51c1cfb211
Cayce House <cayce@caycehouse.com>
2024-05-23 11:14:42 -0400

    archive spt and fika

--- apps/spt/ci/latest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-version=$(curl -sX GET "https://dev.sp-tarkov.com/api/v1/repos/SPT/Server/tags" | jq --raw-output '.[0].name' 2>/dev/null)
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
